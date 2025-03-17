56f6b5ed5cbe4447e16754010b84e53353cb3c9c
João Paulo Dias <joaopdias.dev@gmail.com>
2025-03-17 00:30:53 -0300

    feat: add an environment variable to the CORS policy

--- src/main/java/dev/joaopdias/blog/config/WebConfig.java
+++ src/main/java/dev/joaopdias/blog/config/WebConfig.java
@@ -1,5 +1,6 @@
 package dev.joaopdias.blog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,15 +8,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4000", "http://localhost:4200")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                if (allowedOrigin != null)
+                    registry.addMapping("/**")
+                            .allowedOrigins(allowedOrigin)
+                            .allowedMethods("*")
+                            .allowedHeaders("*");
+                else
+                    registry.addMapping("/**")
+                            .allowedOrigins("http://localhost:4000", "http://localhost:4200")
+                            .allowedMethods("*")
+                            .allowedHeaders("*");
             }
         };
     }
